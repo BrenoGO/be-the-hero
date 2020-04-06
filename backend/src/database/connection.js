f54aa39c9ca44011d59eebaef10d02e4fbc6280e
Breno Oliveira <breno.oliveira.ufv@gmail.com>
2020-04-06 16:48:47 -0300

    adjustments and jest config

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,7 @@
 const knex = require('knex');
 const config = require('../../knexfile');
 
-const connection = knex(config.development);
+const config = process.env.NODE_ENV === 'test' ? config.test : config.development
+const connection = knex(config);
 
 module.exports = connection;
